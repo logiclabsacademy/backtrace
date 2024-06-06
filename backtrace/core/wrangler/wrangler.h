cea85ad8af489a387e11c0311e2d077b20ad9daf
Nnamdi Michael Okpala <logiclabsacademy@protonmail.com>
2024-06-06 23:47:51 +0100

    a

---
+++ backtrace/core/wrangler/wrangler.h
@@ -0,0 +1,14 @@
+// Author : Nnamdi Michael Okpala
+// Description: Wrangler - Code Mangler and Demangler
+
+#ifndef WRANGLER_H
+#define WRANGLER_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdbool.h>
+
+
+#endif
