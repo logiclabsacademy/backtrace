37383452bac8f71386add90a8e0c10ef11091e10
Nnamdi Michael Okpala <logiclabsacademy@protonmail.com>
2024-06-08 22:45:53 +0100

    fixed-tests

--- backtrace/core/wrangler/wrangler.test.c
+++ backtrace/core/wrangler/wrangler.test.c
@@ -7,7 +7,6 @@ int main() {
     const char *text = "const char *code = 'printf(\"Hello, World!\");';";
     double entropy = compute_entropy(text);
     WImage *image = WImage_write_image(entropy, 10, 10);
-    
     print_image(image);
     WImage_free(image);
     return 0;
